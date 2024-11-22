d0090d1ac06ae54dbbd54f6705178081de0628e1
zdrobix <zdroba88@gmail.com>
2024-11-22 23:53:20 +0200

    Created context function to save the logged in user.

--- demo/src/main/java/com/example/demo/service/Context.java
+++ demo/src/main/java/com/example/demo/service/Context.java
@@ -1,4 +1,19 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Utilizator;
+
 public class Context {
+
+    private Utilizator currentUser;
+
+    public Context () {}
+    public Context (Utilizator currentUser) { this.currentUser = currentUser; }
+
+    public Utilizator getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(Utilizator currentUser) {
+        this.currentUser = currentUser;
+    }
 }
