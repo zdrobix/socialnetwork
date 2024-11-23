0527f954bf5d559162cecbef9ff603dfcc934a8a
zdrobix <zdroba88@gmail.com>
2024-11-23 12:59:50 +0200

    Removed useless lines of code.
    Like List<Long> representing friend list, and add/remove methods. These aren't used anymore in result of the db implementation

--- demo/src/main/java/com/example/demo/domain/Utilizator.java
+++ demo/src/main/java/com/example/demo/domain/Utilizator.java
@@ -7,12 +7,10 @@ import java.util.Objects;
 public class Utilizator extends Entity<Long>{
     private String firstName;
     private String lastName;
-    private List<Long> prieteni;
 
     public Utilizator(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.prieteni = new ArrayList<>();
     }
 
     public String getFirstName() {
@@ -31,18 +29,6 @@ public class Utilizator extends Entity<Long>{
         this.lastName = lastName;
     }
 
-    public List<Long> getFriends() {
-        return this.prieteni;
-    }
-
-    public void addFriend(Utilizator friend) {
-        this.prieteni.add(friend.getId());
-    }
-
-    public void removeFriend(Utilizator friend) {
-        this.prieteni.remove(friend.getId());
-    }
-
     @Override
     public String toString() {
         return "Utilizator{" +
@@ -57,12 +43,11 @@ public class Utilizator extends Entity<Long>{
         if (!(o instanceof Utilizator)) return false;
         Utilizator that = (Utilizator) o;
         return getFirstName().equals(that.getFirstName()) &&
-                getLastName().equals(that.getLastName()) &&
-                getFriends().equals(that.getFriends());
+                getLastName().equals(that.getLastName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getFirstName(), getLastName(), getFriends());
+        return Objects.hash(getFirstName(), getLastName());
     }
 }
